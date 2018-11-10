b82998eb5491b2a9f85dc39a0f5c138b2ef790e7
casey crocamo <casey.crocamo@live.com>
2018-11-09 16:56:20 -0800

    setup some stuff

---
+++ IdentityServer/IdentityServer/Config.cs
@@ -0,0 +1,66 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("petstore", "Petstore API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("clientcredentials".Sha512())
+                    },
+                    AllowedScopes = { "petstore" }
+                },
+                new Client
+                {
+                    ClientId = "ro.client",
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                    ClientSecrets =
+                    {
+                        new Secret("resourceowner".Sha512())
+                    }
+                }
+            };
+        }
+
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "alice",
+                    Password = "password"
+                },
+                new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "bob",
+                    Password = "password"
+                }
+            };
+
+        }
+    }
+}
---
+++ IdentityServer/Petshop.Console/Program.cs
@@ -0,0 +1,52 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Petshop.ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+
+        public static async Task MainAsync()
+        {
+            var idServerBase = "https://localhost:5001";
+            var apiBase = "https://localhost:5011";
+            var disco = await DiscoveryClient.GetAsync(idServerBase);
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "clientcredentials");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("petstore");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json + "\n\n");
+
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await client.GetAsync(apiBase + "/api/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+        }
+    }
+}
